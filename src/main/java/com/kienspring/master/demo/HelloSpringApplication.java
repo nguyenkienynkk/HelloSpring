480e5da5747311a8ecd7181ddbeed45a37f5d61a
nguyenkiennk <nguyenkienynk@gmail.com>
2023-02-23 09:47:18 +0700

    update code

---
+++ src/main/java/com/kienspring/master/demo/HelloSpringApplication.java
@@ -0,0 +1,11 @@
+package com.kienspring.master.demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringApplication.class, args);
+	}
+
+}
